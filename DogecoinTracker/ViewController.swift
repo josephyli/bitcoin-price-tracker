14acc177bcf46d24d6510b1e59a6e8f440abaad7
josephyli <josephyli@gmail.com>
2015-10-08 21:28:58 -1000

    I temporarily created a calculator app from Stanford's Swift class

--- DogecoinTracker/ViewController.swift
+++ DogecoinTracker/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var display: UILabel!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var userIsInTheMiddleofTypingaNumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleofTypingaNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleofTypingaNumber = true
+        }
+
+        print("digit = \(digit)")
     }
 
 
