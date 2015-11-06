5b82f20fe9e90e6b4f756ef2ba545c55ec1a000a
josephyli <josephyli@gmail.com>
2015-11-06 00:22:41 -1000

    Test cases for initialization DogecoinTracker class = model part of MVC App icon

--- DogecoinTracker/SettingsViewController.swift
+++ DogecoinTracker/SettingsViewController.swift
@@ -8,6 +8,53 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {    
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
     
+    // UIPicker code : http://codewithchris.com/uipickerview-example/
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [[String]] = [[String]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        // Input data into the Array:
+        pickerData = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+                    ["second", "minute", "hour"]]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    // Catpure the picker view selection
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+        
+    
+    }
+
 }
