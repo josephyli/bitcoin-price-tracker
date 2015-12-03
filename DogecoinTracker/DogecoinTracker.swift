dbdd48560cd82a5ee20f215a063b5b1bc65439f3
tomo21 <tymiura@dhcp-168-105-83-136.wireless.manoa.hawaii.edu>
2015-12-02 15:40:19 -1000

    Cleaned up code.

--- DogecoinTracker/DogecoinTracker.swift
+++ DogecoinTracker/DogecoinTracker.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class DogecoinTracker {
     
-    // Input
+    /* Member variables */
     var min: Double
     var max: Double
     var URL: String
     var cycle: Int
     var currentPrice: Double
-    // Output, used for color
-    var output: CGFloat
-    var oldOutput: Float
-    
+
     /* 
      * Constructor for DogecoinTracker
      * Sets min/max, URL, and cycle
@@ -29,96 +26,35 @@ class DogecoinTracker {
         self.max = max
         self.URL = URL
         self.cycle = cycle
-        self.output = 0.2
-        self.oldOutput = 0.2
         self.currentPrice = 350
-       // getOutput()
         
         if URL.isEmpty || min < 0 || min > max || cycle <= 0 {
             return nil
         }
     }
 
-    /*
-    // Constructor for url and cycle
-    init?(URL: String, cycle:Int) {
-        self.URL = URL
-        self.cycle = cycle
-        self.currentPrice = 300
-        self.output = 0.2
-        self.oldOutput = 0.2
-        self.min = 300
-        self.max = 400
-        self.output = 0.2
-        self.oldOutput = 0.2
-        self.currentPrice = 350
-        
-        if URL.isEmpty || cycle <= 0 {
-            return nil
-        }
-        let defaults = NSUserDefaults.standardUserDefaults()
-        var min:Double = -1
-        min = defaults.doubleForKey("min")
-        let offset:Double = 20
-        if ((min) != -1) { // if min is saved then rest are saved as well
-            let max = defaults.doubleForKey("max")
-            let url = defaults.stringForKey("url")
-            let cycle = defaults.integerForKey("cycle")
-            self.URL = url!
-            self.cycle = cycle
-            // Get current price
-             if min < currentPrice {
-                self.min = min
-            } else {
-                self.min = currentPrice - offset
-            }
-            if max > currentPrice {
-                self.max = max
-            } else {
-                self.max = currentPrice + offset
-            }
-    
-        } else { // No values stored, set min and max from current price
-            self.min = currentPrice - offset
-            self.max = currentPrice + offset
-        }
-    }
-    */
-    
-    /* getter and setter functions */
+    /* gets the min value */
     func getMin() -> Double {
         return min
     }
     
+    /* gets the max value */
     func getMax() -> Double {
         return max
     }
     
+    /* gets the URL */
     func getURL() -> String {
         return URL
     }
     
+    /* gets the cycle value */
     func getCycle() -> Int {
         return cycle
     }
     
-    func setMin(min: Double) {
-        self.min = min
-    }
-    
-    func setMax(max: Double) {
-        self.max = max
-    }
-    
-    func setURL(URL: String) {
-        self.URL = URL
-    }
-    
-    func setCycle(cycle: Int) {
-        self.cycle = cycle
-    }
-    
     /*
+    * Gets the output value
     * Range [0.0, 0.4]
     * 0.0 is red, 0.4 is green
     */
@@ -134,6 +70,27 @@ class DogecoinTracker {
         }
     }
     
+    /* sets the min value */
+    func setMin(min: Double) {
+        self.min = min
+    }
+    
+    /* sets the max value */
+    func setMax(max: Double) {
+        self.max = max
+    }
+    
+    /* sets the URL */
+    func setURL(URL: String) {
+        self.URL = URL
+    }
+    
+    /* sets the cycle value */
+    func setCycle(cycle: Int) {
+        self.cycle = cycle
+    }
+    
+    /* sets the current price */
     func setCurrentPrice(currentPrice: Double) {
         self.currentPrice = currentPrice
     }
@@ -171,10 +128,4 @@ class DogecoinTracker {
         }
         task.resume()
     }
-    
-
-    // Documentation for URL session (seems to be in Obj-C)
-    // https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/URLLoadingSystem/URLLoadingSystem.html#//apple_ref/doc/uid/10000165i
-    // Sample NSURLSession code: https://medium.com/swift-programming/learn-nsurlsession-using-swift-ebd80205f87c
-
 }
